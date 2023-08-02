5ff48cb31a3ea99440bef0027c8360d5deb469f5
kaigwa <kaigwaedwin@gmail.com>
2023-08-02 20:23:28 +0300

    New Feature

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      BBQ-Maandalizi
+<br/>
+
+      <>
+        <label>
+          <input type='text' placeholder='Enter your name'/>
+
+        </label>
+<br/>
+
+        <input type='number' placeholder='Enter your Phone Number'/>
+<br/>
+     
+        <div className='drinks'>
+        Vinywaji:
+        <label>
+          <input type="radio" name="drinks" value="option1" />
+          Juisi
+        </label>
+
+        <label>
+          <input
+            type="radio"
+            name="drinks"
+            value="option2"
+            defaultChecked={true} 
+          />
+          Bia
+        </label>
+        <label>
+          <input type="radio" name="drinks" value="option3" />
+          Kali
+        </label>
+
+        <label>
+          <input type="radio" name="drinks" value="option3" />
+          Wine
+        </label>
+      </div>
+<br/>
+
+      <div className='grill'>
+        Nyama:
+        <label>
+          <input type="radio" name="nyama" value="option1" />
+          Kuku
+        </label>
+
+        <label>
+          <input
+            type="radio"
+            name="myRadio"
+            value="option2"
+            defaultChecked={true} 
+          />
+          Mbuzi
+        </label>
+        <label>
+          <input type="radio" name="nyama" value="option3" />
+          Ng'ombe
+        </label>
+
+        <label>
+          <input type="radio" name="nyama" value="option3" />
+          Kondoo
+        </label>
+
+        <label>
+          <input type="radio" name="nyama" value="option3" />
+          Samaki
+        </label>
+      </div>
+      </>
+      <button>Tuma</button>
     </div>
   );
 }
